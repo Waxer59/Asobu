d371a06924f80fe5ade0069546559f4809989aa1
Waxer59 <78129249+Waxer59@users.noreply.github.com>
2024-09-02 22:58:31 +0200

    Update layout.tsx

--- src/app/(asobu-app)/layout.tsx
+++ src/app/(asobu-app)/layout.tsx
@@ -12,26 +12,7 @@ import '@styles/globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Asobu | AI',
-  description:
-    'Asobu, derived from the Japanese word for "play," is a project that explores the possibilities of AI by integrating various interactive and assistive features. Developed for the Midudev 2024 hackathon, Asobu showcases the versatility of AI by combining music, navigation, translation, education, note-taking, chat, image recognition, and voice commands into a single platform.',
-  icons: {
-    icon: '/favicon.png'
-  },
-  openGraph: {
-    title: 'Asobu | Home',
-    description:
-      'Asobu, derived from the Japanese word for "play," is a project that explores the possibilities of AI by integrating various interactive and assistive features. Developed for the Midudev 2024 hackathon, Asobu showcases the versatility of AI by combining music, navigation, translation, education, note-taking, chat, image recognition, and voice commands into a single platform.',
-    type: 'website',
-    images: [
-      {
-        url: '/asobu-og.png',
-        width: 1200,
-        height: 630,
-        alt: 'og banner'
-      }
-    ]
-  }
+  title: 'Asobu | AI'
 };
 
 export default function AILayout({
